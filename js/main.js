3a5f58f0ef4da41d2fa42ef6f9e718aa26b98191
jubong <twoweekyear2@gmail.com>
2024-07-09 17:27:27 +0900

    배너 자동슬러이더 확인

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  var swiper = new Swiper(".bannerSwiper", {
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+  });
+};

641b7a532b51b319551b7c400a5504e35090e48e
jubong <twoweekyear2@gmail.com>
2024-07-10 16:47:31 +0900

    모달창

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 window.onload = function () {
+  //  모달
+  //배너 슬라이드 기능
   var swiper = new Swiper(".bannerSwiper", {
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
     },
-    loop:true,
+    loop: true,
   });
 };
+
+// 제이쿼리
+$(document).ready(function () {
+  $(".notic").click(function () {
+    $(".modal").addClass("notic");
+  });
+  $(".close-button").click(function () {
+    $(".modal").removeClass("notic");
+  });
+});
